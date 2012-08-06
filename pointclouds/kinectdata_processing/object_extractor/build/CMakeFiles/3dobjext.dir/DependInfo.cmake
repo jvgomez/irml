e8e457170de6afb0333db7e4e9cab6542b98e217
jvgomez <engjotauve@gmail.com>
2012-08-06 02:25:54 +0200

    Improved object extractor

---
+++ pointclouds/kinectdata_processing/object_extractor/build/CMakeFiles/3dobjext.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jabuntu/irml/pointclouds/kinectdata_processing/object_extractor/src/Markup.cpp" "/home/jabuntu/irml/pointclouds/kinectdata_processing/object_extractor/build/CMakeFiles/3dobjext.dir/src/Markup.cpp.o"
+  "/home/jabuntu/irml/pointclouds/kinectdata_processing/object_extractor/src/diranalyzer.cpp" "/home/jabuntu/irml/pointclouds/kinectdata_processing/object_extractor/build/CMakeFiles/3dobjext.dir/src/diranalyzer.cpp.o"
+  "/home/jabuntu/irml/pointclouds/kinectdata_processing/object_extractor/src/file.cpp" "/home/jabuntu/irml/pointclouds/kinectdata_processing/object_extractor/build/CMakeFiles/3dobjext.dir/src/file.cpp.o"
+  "/home/jabuntu/irml/pointclouds/kinectdata_processing/object_extractor/src/main.cpp" "/home/jabuntu/irml/pointclouds/kinectdata_processing/object_extractor/build/CMakeFiles/3dobjext.dir/src/main.cpp.o"
+  "/home/jabuntu/irml/pointclouds/kinectdata_processing/object_extractor/src/scene.cpp" "/home/jabuntu/irml/pointclouds/kinectdata_processing/object_extractor/build/CMakeFiles/3dobjext.dir/src/scene.cpp.o"
+  "/home/jabuntu/irml/pointclouds/kinectdata_processing/object_extractor/src/xmlobject.cpp" "/home/jabuntu/irml/pointclouds/kinectdata_processing/object_extractor/build/CMakeFiles/3dobjext.dir/src/xmlobject.cpp.o"
+  "/home/jabuntu/irml/pointclouds/kinectdata_processing/object_extractor/src/ymlobject.cpp" "/home/jabuntu/irml/pointclouds/kinectdata_processing/object_extractor/build/CMakeFiles/3dobjext.dir/src/ymlobject.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "EIGEN_USE_NEW_STDVECTOR"
+  "EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET"
+  "FLANN_STATIC"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
